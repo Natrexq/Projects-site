44d665bc2b2e7706e15c8538d6c8daba4be3c7c3
Natrexq Natrexq <natanot.yt@gmail.com>
2022-08-23 23:07:36 +0200

    idk what i can write - a lot of work

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,5 @@
 function Main(){
+        let gh = document.getElementById('git');
 
         let but = document.getElementById('hm');
         let conpro = document.getElementById('con_pro');
@@ -9,9 +10,49 @@ function Main(){
         });
     
     
+       
+        let search = document.getElementById('butsrch');
+        let butsrch = document.getElementById('srh');
 
+        const Srch = () => {
+                   
+        let search = document.getElementById('butsrch');
+        let butsrch = document.getElementById('srh');
 
+                if(butsrch.value == ""){
 
+                        if(butsrch.className == "inp_bef"){
+                                butsrch.style.transition = "0.5s ease-in";
+                                butsrch.className = "inp_after";
+                                
+                                
+                        }
+
+                        else if(butsrch.className == "inp_after"){
+                                butsrch.style.transition = "0.5s ease-in";
+                                butsrch.className = "inp_bef";
+        
+                                
+                        }
+
+                }
+                else{
+                        alert("Soon");
+                }
+        
+                      
+                      
+                
+        }
+
+        const Git = () => {
+                window.open("https://github.com/Natrexq");  
+        }
+
+        
+
+        gh.addEventListener('click',Git,false);
+        search.addEventListener('click',Srch,false);
 
 }
 
