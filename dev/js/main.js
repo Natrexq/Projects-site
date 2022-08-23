80e48ce1e4cf07c7d3d631907a66532428797130
Natrexq Natrexq <natanot.yt@gmail.com>
2022-08-23 16:59:44 +0200

    building site part 2

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,7 +1,18 @@
 function Main(){
 
+        let but = document.getElementById('hm');
+        let conpro = document.getElementById('con_pro');
+        let contxt =  document.getElementById('contxt');
+        but.addEventListener('click', function Do(){
+                conpro.style.display = "flex";
+                contxt.style.display = "none";
+        });
+    
+    
+
 
 
 
 }
+
 window.addEventListener('load',Main,false);
