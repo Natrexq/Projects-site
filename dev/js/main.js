75498fc983e0d38255be27a88316c4c3cf53e14b
Natrexq Natrexq <natanot.yt@gmail.com>
2022-08-25 20:32:45 +0200

    style changes 3/3

--- dev/js/main.js
+++ dev/js/main.js
@@ -128,5 +128,77 @@ function Gallery(){
      
 }
 
+function NavBar(){
+
+        const bh = document.querySelector('#bthome');
+        const ba = document.querySelector('#btinfo');
+        const bi = document.querySelector('#btauth');
+        const bc = document.querySelector('#btcont'); 
+
+       bh.addEventListener('click',function(){ CheckNav(1);},false)
+       ba.addEventListener('click',function(){ CheckNav(2);},false)
+       bi.addEventListener('click',function(){ CheckNav(3);},false)
+       bc.addEventListener('click',function(){ CheckNav(4);},false)
+
+       function CheckNav(x){
+
+        if(x == 1){
+              
+                location.reload();
+
+        }
+        else if(x == 2){
+                const info = document.getElementById('info');
+                info.scrollIntoView();
+        }
+        else if(x == 3){
+                const author = document.getElementById('aut');
+                author.scrollIntoView();
+        }
+        else if(x == 4){
+                const contact = document.getElementById('conedn');
+                contact.scrollIntoView();;  
+        }
+
+        
+
+       }
+        
+
+
+
+}
+function SearchSmh(){
+
+        const dwff = document.querySelector('#dwffc');
+        const dwfc = document.querySelector('#dwffc');
+        const adfc = document.querySelector('#dwffc');
+        const dwps = document.querySelector('#dwps');
+
+        const ChcDow = (y) => {
+
+                if(y == 1){
+                        window.open("https://github.com/Natrexq/FileFormatChanger-Website");
+                }
+                else if(y == 2){
+                        window.open("https://github.com/Natrexq/Projects-site");     
+                }
+                else if(y == 3){
+                        window.open("https://github.com/Natrexq/Projects-site");     
+                }
+                else if(y == 4){
+                        window.open("https://github.com/Natrexq/Projects-site");     
+                }
+
+        }
+        dwff.addEventListener('click',function(){ChcDow(1);},false);
+        dwfc.addEventListener('click',function(){ChcDow(2);},false);
+        adfc.addEventListener('click',function(){ChcDow(3);},false);
+        dwps.addEventListener('click',function(){ChcDow(4);},false);
+
+
+}
+window.addEventListener('load',SearchSmh,false);
+window.addEventListener('load',NavBar,false);
 window.addEventListener('load',Gallery,false);
 window.addEventListener('load',Main,false);
